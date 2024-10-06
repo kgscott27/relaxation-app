eadff0ebd12005730864fb1dedecdce77525172b
kgscott27 <keven.scott@outlook.com>
2024-10-06 12:51:36 -0700

    Updates

---
+++ precache-manifest.9ba6f18bee4ade7c4b1123074c398b6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8799355acbdc8eaecd7d144cc050e6c6",
+    "url": "/relaxation-app/index.html"
+  },
+  {
+    "revision": "f3112a597962476c9e8e",
+    "url": "/relaxation-app/static/css/main.1e981502.chunk.css"
+  },
+  {
+    "revision": "c03d4e50b6b3d74c5796",
+    "url": "/relaxation-app/static/js/2.62848f6a.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/relaxation-app/static/js/2.62848f6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee49ca6d5c843bdf6ba",
+    "url": "/relaxation-app/static/js/3.c735291e.chunk.js"
+  },
+  {
+    "revision": "f3112a597962476c9e8e",
+    "url": "/relaxation-app/static/js/main.6fcf0df0.chunk.js"
+  },
+  {
+    "revision": "79d4cf06eb8774506069",
+    "url": "/relaxation-app/static/js/runtime-main.fb1a3554.js"
+  }
+]);
